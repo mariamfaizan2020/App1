4d92c6d3c5625421dda5a42045832f5833ee3b8e
zenkoders <>
2021-12-21 17:32:47 +0500

    agged chat screen

--- main/chat.js
+++ main/chat.js
@@ -1,6 +1,9 @@
 import React,{useState,useCallback} from 'react'
 import { StyleSheet, Text, View ,Platform} from 'react-native'
 import { GiftedChat } from 'react-native-gifted-chat'
+import firebase from 'firebase'
+require('firebase/firestore')
+
 
 
 const chat = () => {
@@ -9,7 +12,48 @@ const chat = () => {
 
     const onSend = useCallback(
         (messages=[]) => {
-            setMessages(previousMessages=>GiftedChat.append(previousMessages,messages))
+            setMessages(previousMessages=>GiftedChat.append(previousMessages,messages));
+            firebase.firestore()
+            .collection('conversation')
+            .add({
+                cid:'',
+                createdAt:'',
+                lastMessage:'',
+                lastMessageTime:'',
+                parties:{
+
+                },
+                partyIds:{
+
+                },
+                partyInfo:{
+
+                },
+                UserId:{
+
+                }
+
+            })
+            .then((snapshot)=>{
+                console.log("snapshoot",snapshot)
+                let conversation =snapshot.docs.map(doc=>{
+                    const data=doc.data();
+                      console.log('data111',data.uid)
+                        return data
+
+            })
+           .doc(data.uid)
+           .collection('messages')
+                   .add({
+                    createdAt: new Date(),
+                    sender:firebase.auth().currentUser?.uid,
+                    text:'',
+                   })
+    
+                })
+
+           
+           
         },
         [],
     )
@@ -17,7 +61,13 @@ const chat = () => {
         <View style={{ flex: 1 }}>
    <GiftedChat 
    messages={messages}
-   onSend={messages=>onSend(messages)}/>
+   onSend={messages=>onSend(messages)}
+   user={{
+    _id:firebase.auth().currentUser?.uid,
+    name:firebase.auth().currentUser?.name,
+
+   }} />
+   
    {
       Platform.OS === 'android' && <KeyboardAvoidingView behavior="padding" />
    }
@@ -27,4 +77,4 @@ const chat = () => {
 
 export default chat
 
-const styles = StyleSheet.create({})
+
--- main/users.js
+++ main/users.js
@@ -1,6 +1,7 @@
 import React, {useState,useEffect} from 'react'
 import { StyleSheet, Text, View ,FlatList,Image,TouchableOpacity} from 'react-native'
 import { FontAwesome } from '@expo/vector-icons'; 
+import {connect} from 'react-redux'
 
 import firebase from 'firebase'
 
@@ -10,16 +11,17 @@ require('firebase/firebase-storage')
 
 
 
-const Users = ({navigation}) => {
+const Users = ({navigation,currentUser},props) => {
     const [users,setUsers]=useState([])
 useEffect(()=>{
     fetchUsers()
-},[])
+},[currentUser])
 
     const fetchUsers=()=>{
-        // console.log("uis",firebase.auth().currentUser.uid)
-
+        // console.log("uis",firebase.auth().currentUser)
+//
         if(firebase.auth().currentUser){
+            console.log("uid",firebase.auth().currentUser)
             firebase.firestore()
             .collection('users')
             .where("uid","!=",firebase.auth().currentUser.uid)
@@ -45,7 +47,83 @@ useEffect(()=>{
         }
        
     }
+    const createConv=(item)=>{
+        
+            firebase.firestore().collection('conversation')
+            .where(`parties.${item.uid}`,'==',true)
+            .where(`parties.${currentUser.uid}`,'==',true)
+            .limit(1)
+            .get()
+            .then((snapshot)=>{
+                if(!snapshot.empty){
+                    console.log('snap:',snapshot)
+                    navigation.navigate('Chat',{
+                        docId:snapshot.docs[0].id,
+                        friendname:item.name,
+                        friendUid:item.uid
+                    })
+                }else{
+                    firebase.firestore()
+                    .collection('conversation')
+                    .add({
+                    
+                        createdAt: new Date(),
+                        // lastMessage:'',
+                        // lastMessageTime:'',
+                        parties:{
+                           [`${item.uid}`]:true,
+                           [`${currentUser.uid}`]:true, 
+                        },
+                        partyIds:[
+                            `${item.uid}`,
+                            `${currentUser.uid}`
+            
+                        ],
+                        partyInfo:{
+                            [`${item.uid}`]:{
+                                name:item.name,
+                                uid:item.uid
+                            },
+                            [`${currentUser.uid}`]:{
+                                name:currentUser.name,
+                                uid:currentUser.uid
+                            }
+                        },
+                        // UserId:
+            
+                        
+            
+                    })
+                .then((res)=>{
+                    console.log('res:',res)
+                    firebase.firestore().collection('conversation')
+                    .doc(res.id)
+                    .update({
+                        cid:res.id
+                    })
+                })
+                
+
+                }
+              
+           
+        })
+            // .doc(data.uid)
+            // .collection('messages')
+            // .add({
+            //     createdAt: new Date(),
+            //         sender:firebase.auth().currentUser?.uid,
+            //         text:'',
+            // })
+            
+           
+        }
+
+        
+       
 console.log("users",users)
+console.log("users",props)
+console.log("currentUser",currentUser)
 
     
     return (
@@ -61,7 +139,7 @@ console.log("users",users)
            renderItem={({item})=>{
                console.log("image",item)
                return(
-                   <TouchableOpacity onPress={()=>navigation.navigate('Chat')}>
+                   
                         <View style={{backgroundColor:'lightgrey',padding:10,margin:10,flexDirection:'row'}}>
                     <View >
                      {item.image ? <Image source={{ uri: item.image }} style={{width:50,height:50,borderRadius:360}} /> : <FontAwesome name="user" size={50} color= '#0798f2'/>}
@@ -69,11 +147,15 @@ console.log("users",users)
                    <View style={{justifyContent:'center',alignItems:'center',paddingLeft:10}}>
                    {/* <Text> {item.name}</Text> */}
                    <Text>{item.email}</Text>
+                   <TouchableOpacity onPress={()=>{createConv(item)}}>
+                   <Text>Chat</Text>
+                       </TouchableOpacity>
+                 
                    {/* <Text>{item.phoneNo}</Text> */}
                    </View>
                    </View>
 
-                   </TouchableOpacity>
+                 
 
               
            
@@ -90,10 +172,15 @@ console.log("users",users)
 
 
 
-const styles = StyleSheet.create({})
+const mapStateToProps=(store)=>{
+    console.log("store",store)
+    return {
+        currentUser:store.userState.currentUser
+    }}
+        
+export default connect(mapStateToProps,null)(Users);
 
 
 
-export default Users
 
 
